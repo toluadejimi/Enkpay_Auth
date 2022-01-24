999e3f80d42211c46a5792a6cbe6773ea4514540
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-01-24 11:34:47 +0100

    update api route

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,28 @@
 <?php
+/** API Routes */
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\RegisterController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/** Registration user (phone, password) */
+/** Verify token sent (token) */
+// By text token or By voice token (text is default)
+
+/** Authentication (login, logout) */
+
+/** Forgot request password (phone) */
+/** Verify reset password token sent (token) */
+/** Reset password (password)*/
+
+/** Register routes */
+Route::controller(RegisterController::class)
+    ->prefix('auth/register')
+    ->as('auth.')
+    ->group(function () {
+       Route::post('/', 'register')->name('register');
+    });
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
