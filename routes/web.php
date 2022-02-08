e4ff234219c94b10bc4cf44c09b60a1090537fe2
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-02-08 13:56:44 +0100

    update: User virtual account, create and notification sent

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\LandingPageController;
 
 /** Landing page route */
-//Route::get('/', LandingPageController::class);
-
-
-Route::get('/', function () {
-   $state = \App\Services\Bank\Vulte\Reference::signature("RQ|20220208120217|742648");
-
-   dd($state);
-});
+Route::get('/', LandingPageController::class);
