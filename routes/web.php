b035ba80516817164de73cb0fe0556280d5d27a8
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-04-07 10:16:48 +0100

    fix pin encryption bug

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,3 @@ use App\Http\Controllers\Web\LandingPageController;
 
 /** Landing page route */
 Route::get('/', LandingPageController::class);
-
-Route::get('/frame', function (){
-    $user = Auth::loginUsingId(1);
-    dd(\Illuminate\Support\Facades\Crypt::decrypt($user->pin));
-    //return \Illuminate\Support\Facades\Crypt::encrypt('806080');
-});
