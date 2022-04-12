020786462bef9c1248608a3c5548b722a38a1ba1
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-04-12 09:06:22 +0100

    refactoring response format for sent verification token

--- app/Http/Controllers/Api/NewVerificationTokenController.php
+++ app/Http/Controllers/Api/NewVerificationTokenController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\NewVerificationTokenRequest;
 use App\Notifications\SendVerificationTokenNotification;
+use Symfony\Component\HttpFoundation\Response;
 
 class NewVerificationTokenController extends BaseApiController
 {
@@ -12,6 +13,14 @@ class NewVerificationTokenController extends BaseApiController
     {
         $request->expectedUser()->notify(new SendVerificationTokenNotification());
 
-        return $this->sendResponse([], 'Verification token sent');
+        return response()->json([
+            'success' => true,
+            'errors' => null,
+            'message' => 'Verification token sent',
+            'data' => null
+        ])->setStatusCode(
+            Response::HTTP_OK,
+            Response::$statusTexts[Response::HTTP_OK]
+        );
     }
 }
