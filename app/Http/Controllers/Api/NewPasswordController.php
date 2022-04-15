84cd0725cf1489a6fd63412cae534624c8d2d67d
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-04-15 11:42:12 +0100

    refactor change password

--- app/Http/Controllers/Api/NewPasswordController.php
+++ app/Http/Controllers/Api/NewPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\ResetPasswordRequest;
 use App\Actions\Account\UpdatePasswordAction;
+use Symfony\Component\HttpFoundation\Response;
 
 class NewPasswordController extends BaseApiController
 {
@@ -12,8 +13,26 @@ class NewPasswordController extends BaseApiController
     {
         $state = UpdatePasswordAction::execute($request->toArray());
 
-        return $state
-            ? $this->sendResponse([], 'Password was successfully updated')
-            : $this->sendError('Unable to update password.');
+        if ($state) {
+            return response()->json([
+                'success' => true,
+                'errors' => '',
+                'message' => 'Password was successfully updated.',
+                'data' => null
+            ])->setStatusCode(
+                Response::HTTP_OK,
+                Response::$statusTexts[Response::HTTP_OK]
+            );
+        }
+
+        return response()->json([
+            'success' => true,
+            'errors' => 'Invalid',
+            'message' => 'Unable to update password, Please try again later.',
+            'data' => null
+        ])->setStatusCode(
+            Response::HTTP_OK,
+            Response::$statusTexts[Response::HTTP_OK]
+        );
     }
 }
