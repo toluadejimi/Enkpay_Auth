7f36ae5bb0409c5f045de465e01ced407e1f0574
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-04-15 12:16:43 +0100

    update

--- app/Http/Controllers/Api/Account/VerifyPinController.php
+++ app/Http/Controllers/Api/Account/VerifyPinController.php
@@ -3,14 +3,48 @@
 namespace App\Http\Controllers\Api\Account;
 
 use App\Http\Requests\User\VerifyPinRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class VerifyPinController
 {
     public function __invoke(VerifyPinRequest $request)
     {
+        $state = '';
+
+        try {
+            $state = $request->attempt();
+        }catch(\Exception $exception) {
+            return response()->json([
+                'success' => true,
+                'errors' => 'Invalid transaction pin',
+                'message' => 'Your transaction pin was invalid, please try again.',
+                'data' => null
+            ])->setStatusCode(
+                Response::HTTP_OK,
+                Response::$statusTexts[Response::HTTP_OK]
+            );
+        }
+
+        if ($state) {
+            return response()->json([
+                'success' => true,
+                'errors' => '',
+                'message' => 'Transaction pin is valid',
+                'data' => null
+            ])->setStatusCode(
+                Response::HTTP_OK,
+                Response::$statusTexts[Response::HTTP_OK]
+            );
+        }
+
         return response()->json([
-            'status' => $state = $request->attempt(),
-            'message' => $state ? 'Transaction was valid' : 'Invalid transaction pin'
-        ]);
+            'success' => true,
+            'errors' => 'Invalid transaction pin',
+            'message' => 'Your transaction pin was invalid, please try again.',
+            'data' => null
+        ])->setStatusCode(
+            Response::HTTP_OK,
+            Response::$statusTexts[Response::HTTP_OK]
+        );
     }
 }
