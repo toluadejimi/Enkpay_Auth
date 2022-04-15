1d9fce2e626df99ea9b646046a7244fdd2837bdc
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-04-15 11:59:55 +0100

    refactor profile controller

--- app/Http/Controllers/Api/Account/ProfileController.php
+++ app/Http/Controllers/Api/Account/ProfileController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\Api\Account;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
+use Propaganistas\LaravelPhone\PhoneNumber;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProfileController extends Controller
 {
@@ -12,6 +13,25 @@ class ProfileController extends Controller
     {
         $user = Auth::user();
 
-        return new UserResource($user);
+        return response()->json([
+            'success' => true,
+            'errors' => '',
+            'message' => 'Your account profile',
+            'data' => [
+                'id' => $user->uuid,
+                'name' => $user->full_name,
+                'phone_number' => PhoneNumber::make(
+                    $user->phone,
+                    $user->phone_country
+                )->formatE164(),
+                'account_type' => $user->type,
+                'pin_status' => $user->pin_status,
+                'account_number' => $user->account_number,
+                'account_balance' => $user->virtual_account_balance,
+            ]
+        ])->setStatusCode(
+            Response::HTTP_OK,
+            Response::$statusTexts[Response::HTTP_OK]
+        );
     }
 }
