de7e25d19a2657aefb8cebcf6bd9be06b438571e
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-04-15 11:57:02 +0100

    refactor transaction update

--- app/Http/Controllers/Api/Transaction/UpdateTransactionPinController.php
+++ app/Http/Controllers/Api/Transaction/UpdateTransactionPinController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Transaction;
 
+use Symfony\Component\HttpFoundation\Response;
 use App\Actions\Transaction\UpdateTransactionPinAction;
 use App\Http\Requests\Transaction\UpdateTransactionPinRequest;
 
@@ -9,13 +10,31 @@ class UpdateTransactionPinController
 {
     public function __invoke(UpdateTransactionPinRequest $request)
     {
-        $state = UpdateTransactionPinAction::execute(
-            $request->validated()
-        );
+        try {
+            UpdateTransactionPinAction::execute(
+                $request->validated()
+            );
 
-        return response()->json([
-            'status' => $state,
-            'message' => $state ? 'Transaction pin successfully updated' : 'Unable to update transaction pin'
-        ]);
+            return response()->json([
+                'success' => true,
+                'errors' => '',
+                'message' => 'Transaction pin successfully updated.',
+                'data' => null
+            ])->setStatusCode(
+                Response::HTTP_OK,
+                Response::$statusTexts[Response::HTTP_OK]
+            );
+
+        }catch (\Exception $exception) {
+            return response()->json([
+                'success' => true,
+                'errors' => 'Unable to update transaction pin.',
+                'message' => 'Encounter an error why trying to update transaction pin, please try again later.',
+                'data' => null
+            ])->setStatusCode(
+                Response::HTTP_OK,
+                Response::$statusTexts[Response::HTTP_OK]
+            );
+        }
     }
 }
