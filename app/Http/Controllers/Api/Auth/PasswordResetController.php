aafa04837ca20132fc712faa0d8d8d8e436fb6fa
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-04-15 11:49:14 +0100

    refactor password reset

--- app/Http/Controllers/Api/Auth/PasswordResetController.php
+++ app/Http/Controllers/Api/Auth/PasswordResetController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use Illuminate\Http\JsonResponse;
-use App\Http\Resources\UserResource;
+use Symfony\Component\HttpFoundation\Response;
 use App\Http\Controllers\Api\BaseApiController;
 use App\Http\Requests\NewVerificationTokenRequest;
 use App\Notifications\SendVerificationTokenNotification;
@@ -14,10 +14,45 @@ class PasswordResetController extends BaseApiController
     {
         $user = $request->expectedUser();
 
-        $user->generateVerificationToken();
+        if ($user->exists) {
 
-        $user->notify(new SendVerificationTokenNotification());
+            try{
+                $user->generateVerificationToken();
+                $user->notify(new SendVerificationTokenNotification());
 
-        return $this->sendResponse(['id' => $user->uuid], 'A Verification token has been sent');
+                return response()->json([
+                    'success' => true,
+                    'errors' => '',
+                    'message' => 'Verification token has been sent.',
+                    'data' => [
+                        'id' => $user->uuid
+                    ]
+                ])->setStatusCode(
+                    Response::HTTP_OK,
+                    Response::$statusTexts[Response::HTTP_OK]
+                );
+            }catch (\Exception $exception) {
+                return response()->json([
+                    'success' => true,
+                    'errors' => 'Unable to send verification token',
+                    'message' => 'An error was encounter while sending the verification token, please try again.',
+                    'data' => null
+                ])->setStatusCode(
+                    Response::HTTP_OK,
+                    Response::$statusTexts[Response::HTTP_OK]
+                );
+            }
+
+        }
+
+        return response()->json([
+            'success' => true,
+            'errors' => 'Unable to send verification token',
+            'message' => 'We unable to send verification token because user was not found.',
+            'data' => null
+        ])->setStatusCode(
+            Response::HTTP_OK,
+            Response::$statusTexts[Response::HTTP_OK]
+        );
     }
 }
