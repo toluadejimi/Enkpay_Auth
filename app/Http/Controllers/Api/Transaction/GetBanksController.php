d51753e4a112de1dbfec7262d43ef183a622ba8c
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-04-15 11:37:21 +0100

    refactor bank controller

--- app/Http/Controllers/Api/Transaction/GetBanksController.php
+++ app/Http/Controllers/Api/Transaction/GetBanksController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Transaction;
 use Illuminate\Support\Facades\Auth;
 use App\Actions\Transaction\GetBanksAction;
 use Illuminate\Http\Client\RequestException;
+use Symfony\Component\HttpFoundation\Response;
 
 class GetBanksController
 {
@@ -14,11 +15,14 @@ class GetBanksController
             $banks = GetBanksAction::execute(Auth::user());
         }catch (\Exception $exception){
             return response()->json([
-                'success' => false,
-                'errors' => true,
+                'success' => true,
+                'errors' => 'Server error',
                 'message' => 'Unable to get bank data.',
-                'data' => []
-            ]);
+                'data' => null
+            ])->setStatusCode(
+                Response::HTTP_OK,
+                Response::$statusTexts[Response::HTTP_OK]
+            );
         }
 
         return response()->json([
@@ -26,6 +30,9 @@ class GetBanksController
             'errors' => '',
             'message' => 'List of banks',
             'data' => $banks
-        ]);
+        ])->setStatusCode(
+            Response::HTTP_OK,
+            Response::$statusTexts[Response::HTTP_OK]
+        );
     }
 }
