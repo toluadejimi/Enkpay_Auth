136fcf489771fa517a8b8b707f562b1b1b65e30a
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2022-05-10 11:39:54 +0100

    update transaction type

--- app/Http/Requests/Transaction/DebitRequest.php
+++ app/Http/Requests/Transaction/DebitRequest.php
@@ -6,14 +6,24 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class DebitRequest extends FormRequest
 {
+    protected array $transactionType = [
+        'airtime', 'data', 'in-transfer', 'out-transfer',
+        'cash-in', 'dstv', 'gotv', 'startime', 'flight', 'electric',
+        'education', 'insurance', 'exchange'
+    ];
     public function rules(): array
     {
         return [
             'uuid' => ['required', 'string', 'exists:users,uuid'],
-            'transaction_type' => ['required','string'],
+            'transaction_type' => ['required','string', 'in:'.$this->getTransactionTypeArray()],
             'amount' => ['required', 'string'],
             'description' => ['sometimes', 'required', 'string', 'max:255']
         ];
+    }
+
+    protected function getTransactionTypeArray(): string
+    {
+        return implode(',', $this->transactionType);
     }
 
     public function authorize(): bool
